3b204feeeae90ef5589e11d288b27ddbf52f1a63
applesniperz <ngoseng_gee@live.com.sg>
2023-12-02 19:25:38 +0800

    SengGee's copy

---
+++ ros2_ws/src/learning_tf2_py/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'learning_tf2_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='johnsmith',     
+    maintainer_email='jsmith@todo.todo',     
+    description='This is the package description for Task 5',     
+    license='Apache License 2.0', 
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'static_turtle_tf2_broadcaster = learning_tf2_py.static_turtle_tf2_broadcaster:main',
+        'turtle_tf2_broadcaster = learning_tf2_py.turtle_tf2_broadcaster:main',
+        'turtle_tf2_listener = learning_tf2_py.turtle_tf2_listener:main',
+        ],
+    },
+)
