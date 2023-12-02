9fd419023b9384d79dddcd22321b0967945c3967
applesniperz <ngoseng_gee@live.com.sg>
2023-12-02 19:27:12 +0800

    undo

--- ros2_ws/build/action_tutorials_py/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ngo/ros2_ws/install/action_tutorials_interfaces/share/action_tutorials_interfaces/package.sh"
--- ros2_ws/build/action_tutorials_py/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/ngo/ros2_ws/install/action_tutorials_py'
